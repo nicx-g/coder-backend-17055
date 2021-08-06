dfbecf2e51c776e287a17b8badf8af224ee47db1
Nicolás Gomez <nicx_g@hotmail.com>
2021-08-06 00:09:10 -0300

    order change

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
+const app = express();
 import express from 'express';
+const port = 8080;
 import Products from './class';
 const products = new Products();
-const port = 8080;
-const app = express();
 
 const server = app.listen(port, () => console.log(`Server running in port:  ${port}`));
 server.on('error', (err) => console.error(`There was an error: ${err}`));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
 app.get('/api/productos/listar', (req, res) => {
     const arrayOfProducts: any = products.getProducts();
     if(arrayOfProducts.error) res.status(404).json({error: arrayOfProducts.error})
